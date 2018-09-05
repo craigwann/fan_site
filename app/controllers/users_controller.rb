3f56526de28bac9061ae2c3282475103810919e5
craig wann and eric shull <craig@>
2018-09-05 16:41:26 -0700

    add edit form and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+    render :edit
+  end
+
   private
   def user_params
     params.require(:user).permit(:name)
