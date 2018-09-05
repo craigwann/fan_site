258b72306ef4ed58193f425aa0b762a063060035
craig wann and eric shull <eric@>
2018-09-05 16:46:26 -0700

    add update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,16 @@ class UsersController < ApplicationController
     render :edit
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(list_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+
+  end
+
   private
   def user_params
     params.require(:user).permit(:name)
